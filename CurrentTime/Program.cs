8bc7c48a9c77efd1179eeab38eef2be90a9e4536
agrawalraj02 <82352694+agrawalraj02@users.noreply.github.com>
2025-01-10 19:23:05 +0530

    c

--- CurrentTime/Program.cs
+++ CurrentTime/Program.cs
@@ -12,6 +12,7 @@ namespace CurrentTime
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+             
 
 
             var app = builder.Build();            
